99381a8ec95c8b99733f30a55133a91ce1679c50
dexter <dexter@nexondv.com>
2022-11-30 16:50:54 +0700

    update enemies, player, background, movement in final project

--- final-game/player.js
+++ final-game/player.js
@@ -1,17 +1,34 @@
+import { Sitting, Running, Jumping, Falling } from "./playerStates.js";
+
 export class Player {
   constructor(game) {
     this.game = game;
     this.width = 100;
     this.height = 91.3;
     this.x = 0;
-    this.y = this.game.height - this.height;
+    this.y = this.game.height - this.height - this.game.groundMargin;
     this.vy = 0;
     this.weight = 1;
     this.image = player;
+    this.frameX = 0;
+    this.frameY = 0;
+    this.maxFrame;
+    this.fps = 20;
+    this.frameInterval = 1000 / this.fps;
+    this.frameTimer = 0;
     this.speed = 0;
-    this.maxSpeed = 10;
+    this.maxSpeed = 6;
+    this.states = [
+      new Sitting(this),
+      new Running(this),
+      new Jumping(this),
+      new Falling(this),
+    ];
+    this.currentState = this.states[0];
+    this.currentState.enter();
   }
-  update(input) {
+  update(input, deltaTime) {
+    this.currentState.handleInput(input);
     // horizontal movement
     this.x += this.speed;
     if (input.includes("ArrowRight")) this.speed = this.maxSpeed;
@@ -21,16 +38,21 @@ export class Player {
     if (this.x > this.game.width - this.width)
       this.x = this.game.width - this.width;
     // vertical movement
-    if (input.includes("ArrowUp") && this.onGround()) this.vy -= 20;
     this.y += this.vy;
     if (!this.onGround()) this.vy += this.weight;
     else this.vy = 0;
+    // sprite animation
+    if (this.frameTimer > this.frameInterval) {
+      this.frameTimer = 0;
+      if (this.frameX < this.maxFrame) this.frameX++;
+      else this.frameX = 0;
+    } else this.frameTimer += deltaTime;
   }
   draw(context) {
     context.drawImage(
       this.image,
-      0,
-      0,
+      this.frameX * this.width,
+      this.frameY * this.height,
       this.width,
       this.height,
       this.x,
@@ -40,6 +62,11 @@ export class Player {
     );
   }
   onGround() {
-    return this.y >= this.game.height - this.height;
+    return this.y >= this.game.height - this.height - this.game.groundMargin;
+  }
+  setState(state, speed) {
+    this.currentState = this.states[state];
+    this.game.speed = this.game.maxSpeed * speed;
+    this.currentState.enter();
   }
 }
